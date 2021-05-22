1c7903d430fdd1120f62ea163784d63c07670be4
Kohei Suzuki <eixecosxpisinx@gmail.com>
2021-05-22 21:57:40 +0900

    Add include directory

--- include/sparse_matrix.hpp
+++ include/sparse_matrix.hpp
@@ -5,8 +5,10 @@
 //  Created by Kohei Suzuki on 2021/05/22.
 //
 
-#ifndef sparse_matrix_h
-#define sparse_matrix_h
+#ifndef sparse_matrix_hpp
+#define sparse_matrix_hpp
+
+#include "../src/sparse_matrix/compressed_row_storage.hpp"
 
 
-#endif /* sparse_matrix_h */
+#endif /* sparse_matrix_hpp */
