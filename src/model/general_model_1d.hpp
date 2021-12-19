f4f442349f3b9312a482513ee5c0f1746dd5bf66
Kohei Suzuki <eixecosxpisinx@gmail.com>
2021-12-20 04:07:11 +0900

    Add docs

--- src/model/general_model_1d.hpp
+++ src/model/general_model_1d.hpp
@@ -16,15 +16,24 @@
 namespace compnal {
 namespace model {
 
+//! @brief The general model class for one-dimensional systems.
+//! @tparam BaseClass The base model classes.
 template<class BaseClass>
 class GeneralModel_1D: public BaseClass {
    
+   //! @brief The type of real values.
    using RealType = typename BaseClass::ValueType;
+   
+   //! @brief Alias of compressed row strage (CRS) with RealType.
    using CRS = sparse_matrix::CRS<RealType>;
    
 public:
    using BaseClass::BaseClass;
    
+   //! @brief Add an onsite potential term to the system \f$ c\times \hat{O}_{i} \f$.
+   //! @param val The coefficient of the potential term \f$ c \f$.
+   //! @param m The onsite operator \f$ \hat{O}_{i} \f$.
+   //! @param site The site \f$ i\f$.
    void AddOnsitePotential(const RealType val, const CRS &m, const int site) {
       if (val == 0.0) {
          return;
@@ -43,10 +52,19 @@ public:
       }
    }
    
+   //! @brief Add an onsite potential term to the system \f$ \hat{O}_{i} \f$.
+   //! @param m The onsite operator \f$ \hat{O}_{i} \f$.
+   //! @param site The site \f$ i\f$.
    void AddOnsitePotential(const CRS &m, const int site) {
       AddOnsitePotential(1.0, m, site);
    }
    
+   //! @brief Add an interaction term to the system \f$ c\times\hat{O}_{i}\hat{P}_{j} \f$.
+   //! @param val The coefficient of the interaction term \f$ c \f$.
+   //! @param m_1 The onsite operator \f$ \hat{O}_{i} \f$.
+   //! @param site_1 The site \f$ i\f$.
+   //! @param m_2 The onsite operator \f$ \hat{P}_{j} \f$.
+   //! @param site_2 The site \f$ j\f$.
    void AddInteraction(const RealType val, const CRS &m_1, const int site_1, const CRS &m_2, const int site_2) {
       if (val == 0.0) {
          return;
@@ -71,17 +89,39 @@ public:
       intersite_operator_list_[site_1][site_2].push_back({m_1.MultiplyByScalar(val), m_2});
    }
    
+   //! @brief Add an interaction term to the system \f$ \hat{O}_{i}\hat{P}_{j} \f$.
+   //! @param m_1 The onsite operator \f$ \hat{O}_{i} \f$.
+   //! @param site_1 The site \f$ i\f$.
+   //! @param m_2 The onsite operator \f$ \hat{P}_{j} \f$.
+   //! @param site_2 The site \f$ j\f$.
    void AddInteraction(const CRS &m_1, const int site_1, const CRS &m_2, const int site_2) {
       AddInteraction(1.0, m_1, site_1, m_2, site_2);
    }
    
+   //! @brief Get onsite potential term list added to the system.
+   //! @return Onsite potential term list added to the system.
    inline const std::unordered_map<int, CRS> &GetOnsiteOperatorList() const { return onsite_operator_list_; }
+   
+   //! @brief Get an onsite potential term added to the system \f$ \hat{O}_{i}\f$.
+   //! @param site The site \f$ i\f$.
+   //! @return The onsite potential term added to the system \f$ \hat{O}_{i}\f$.
    inline const CRS &GetOnsiteOperator(const int site) const { return onsite_operator_list_.at(site); }
+   
+   //! @brief Get interaction term list added to the system.
+   //! @return Interaction term list added to the system.
    inline const std::unordered_map<int, std::unordered_map<int, std::vector<std::pair<CRS, CRS>>>> &GetIntersiteOperatorList() const { return intersite_operator_list_; }
+   
+   //! @brief Get an interaction term added to the system \f$ \hat{O}_{i}\hat{P}_{j}\f$.
+   //! @param site_1 The site \f$ i\f$.
+   //! @param site_2 The site \f$ j\f$.
+   //! @return The interaction term added to the system \f$ \hat{O}_{i}\f$ and \f$ \hat{P}_{j}\f$.
    inline const std::vector<std::pair<CRS, CRS>> &GetIntersiteOperator(const int site_1, const int site_2) const { return intersite_operator_list_.at(site_1).at(site_2); }
    
 private:
+   //! @brief Onsite potential term list added to the system.
    std::unordered_map<int, CRS> onsite_operator_list_;
+   
+   //! @brief Interaction term list added to the system.
    std::unordered_map<int, std::unordered_map<int, std::vector<std::pair<CRS, CRS>>>> intersite_operator_list_;
    
 };
