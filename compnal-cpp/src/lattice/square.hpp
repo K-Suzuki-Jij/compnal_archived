f80e720115d9701cf669f2271d62554d17dbe741
KSuzuki <eixecosxpisinx@gmail.com>
2022-08-11 14:00:37 +0900

    Add square class

--- compnal-cpp/src/lattice/square.hpp
+++ compnal-cpp/src/lattice/square.hpp
@@ -13,15 +13,95 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //
-//  Square.hpp
+//  square.hpp
 //  compnal
 //
 //  Created by kohei on 2022/08/10.
 //  
 //
 
-#ifndef Square_h
-#define Square_h
+#ifndef COMPNAL_LATTICE_SQUARE_HPP_
+#define COMPNAL_LATTICE_SQUARE_HPP_
+
+#include "boundary_condition.hpp"
+#include <stdexcept>
+
+namespace compnal {
+namespace lattice {
+
+class Square {
+   
+public:
+   
+   Square(const int x_size, const int y_size) {
+      SetSystemSize(x_size, y_size);
+   }
+   
+   Square(const int x_size, const int y_size, const BoundaryCondition boundary_condition) {
+      SetSystemSize(x_size, y_size);
+      SetBoundaryCondition(boundary_condition);
+   }
+   
+   void SetXSize(const int x_size) {
+      if (x_size < 0) {
+         throw std::runtime_error("x_size must be larger than or equal to 0.");
+      }
+      x_size_ = x_size;
+   }
+   
+   void SetYSize(const int y_size) {
+      if (y_size < 0) {
+         throw std::runtime_error("y_size must be larger than or equal to 0.");
+      }
+      y_size_ = y_size;
+   }
+   
+   void SetSystemSize(const int x_size, const int y_size) {
+      SetXSize(x_size);
+      SetYSize(y_size);
+   }
+   
+   //! @brief Set boundary condition.
+   //! @param boundary_condition Boundary condtion.
+   void SetBoundaryCondition(const BoundaryCondition boundary_condition) {
+      if (boundary_condition == BoundaryCondition::NONE) {
+         throw std::runtime_error("BoundaryCondition::NONE cannot be set.");
+      }
+      bc_ = boundary_condition;
+   }
+   
+   int GetXSize() const {
+      return x_size_;
+   }
+   
+   int GetYSize() const {
+      return y_size_;
+   }
+   
+   //! @brief Get system size.
+   //! @return System size.
+   int GetSystemSize() const {
+      return x_size_*y_size_;
+   }
+   
+   //! @brief Get boundary condition.
+   //! @return Boundary condition.
+   BoundaryCondition GetBoundaryCondition() const {
+      return bc_;
+   }
+   
+private:
+   int x_size_ = 0;
+   int y_size_ = 0;
+   
+   //! @brief Boundary condition.
+   BoundaryCondition bc_ = BoundaryCondition::OBC;
+};
 
 
-#endif /* Square_h */
+} // namespace lattice
+} // namespace compnal
+
+
+
+#endif /* COMPNAL_LATTICE_SQUARE_HPP_ */
