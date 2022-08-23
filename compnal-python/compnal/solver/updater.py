66a938d4dcfeb0eea12e91b4894d4413e6f81e1b
KSuzuki <eixecosxpisinx@gmail.com>
2022-08-24 01:10:37 +0900

    Add comments

--- compnal-python/compnal/solver/updater.py
+++ compnal-python/compnal/solver/updater.py
@@ -2,10 +2,23 @@ from enum import Enum
 from base_compnal import base_solver
 
 class Updater(Enum):
+    """Update algorithm.
+    """
     METROPOLIS = 0
     HEAT_BATH = 1
 
-def cast_base_updater(updater: base_solver.CMCUpdater):
+def cast_base_updater(updater: base_solver.CMCUpdater) -> Updater:
+    """Cast the updater from base_solver.CMCUpdater to solver.Updater.
+
+    Args:
+        updater (base_solver.CMCUpdater): The updater as base_solver.CMCUpdater.
+
+    Raises:
+        RuntimeError: When the unknonw updater is input.
+
+    Returns:
+        Updater: The updater as solver.Updater.
+    """
     if updater == base_solver.CMCUpdater.METROPOLIS:
         return Updater.METROPOLIS
     elif updater == base_solver.CMCUpdater.HEAT_BATH:
@@ -14,6 +27,17 @@ def cast_base_updater(updater: base_solver.CMCUpdater):
         raise RuntimeError("Unknonw updater")
 
 def cast_updater(updater: Updater):
+    """Cast the updater from solver.Updater to base_solver.CMCUpdater.
+
+    Args:
+        updater (Updater): The updater as solver.Updater.
+
+    Raises:
+        RuntimeError: When the unknonw updater is input.
+
+    Returns:
+        base_solver.CMCUpdater: The updater as base_solver.CMCUpdater.
+    """
     if updater == Updater.METROPOLIS:
         return base_solver.CMCUpdater.METROPOLIS
     elif updater == Updater.HEAT_BATH:
